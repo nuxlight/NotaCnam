3ea547b66a343d1251141cd77a8038973b2eacee
Thibaud Pellissier (Ynover) <thibaud.pellissier@ynover.com>
2015-11-05 14:58:26 +0100

    Add tab activity on app
    Signed-off-by: Thibaud Pellissier (Ynover) <thibaud.pellissier@ynover.com>

--- app/src/main/java/trashcompagnie/notacnam/MainActivity.java
+++ app/src/main/java/trashcompagnie/notacnam/MainActivity.java
@@ -1,9 +1,11 @@
 package trashcompagnie.notacnam;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -34,5 +36,16 @@ public class MainActivity extends AppCompatActivity {
             dialog.show();
         }
 
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!labelOne.getText().equals("") && !labelTow.getText().equals("")){
+                    Intent intent = new Intent(getApplicationContext(), NotesActivity.class);
+                    intent.putExtra("compte", labelOne.getText().toString());
+                    intent.putExtra("audit", labelTow.getText().toString());
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
