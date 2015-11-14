b301e452c5010998dd5b37945feb32c730469f18
Thibaud Pellissier <thibaudpellissier@gmail.com>
2015-11-14 16:00:50 +0100

    Update LICENCE file
    Signed-off-by: Thibaud Pellissier <thibaudpellissier@gmail.com>

--- app/src/main/java/trashcompagnie/notacnam/NotesActivity.java
+++ app/src/main/java/trashcompagnie/notacnam/NotesActivity.java
@@ -2,6 +2,7 @@ package trashcompagnie.notacnam;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ArrayAdapter;
@@ -12,10 +13,14 @@ import org.json.simple.JSONArray;
 
 import java.util.ArrayList;
 
-public class NotesActivity extends AppCompatActivity implements AuthClassLisner {
+public class NotesActivity extends AppCompatActivity implements AuthClassLisner, SwipeRefreshLayout.OnRefreshListener {
 
     private ListView notesListe;
     private ProgressDialog loadDialog;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private AuthClass authTools;
+    private String compte_id;
+    private String code_auditeur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +28,30 @@ public class NotesActivity extends AppCompatActivity implements AuthClassLisner 
         setContentView(R.layout.activity_notes);
 
         Bundle bundle = getIntent().getExtras();
-        String compte_id = bundle.getString("compte");
-        String code_auditeur = bundle.getString("audit");
+        compte_id = bundle.getString("compte");
+        code_auditeur = bundle.getString("audit");
 
         //Create Progress dialog
         loadDialog = new ProgressDialog(this);
         loadDialog.setMessage("Chargement des notes en cours...");
         loadDialog.setCancelable(false);
 
-        AuthClass authTools = new AuthClass(NotesActivity.this);
+        authTools = new AuthClass(NotesActivity.this);
         String[] parms = {compte_id, code_auditeur};
         authTools.execute(parms);
         loadDialog.show();
         notesListe = (ListView) findViewById(R.id.liste_notes);
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(this);
+    }
+
+
+    @Override
+    public void onRefresh() {
+        authTools = new AuthClass(NotesActivity.this);
+        String[] parms = {compte_id, code_auditeur};
+        authTools.execute(parms);
     }
 
     @Override
@@ -47,7 +63,10 @@ public class NotesActivity extends AppCompatActivity implements AuthClassLisner 
         Log.i(getClass().getName(), "it's back lenght : " + updateNewArray.length);
         ListAdapter arrayAdapter = new NotaArrayAdapteur(getApplicationContext(),updateNewArray);
         notesListe.setAdapter(arrayAdapter);
-        loadDialog.dismiss();
+        if (loadDialog.isShowing()){
+            loadDialog.dismiss();
+        }
+        swipeRefreshLayout.setRefreshing(false);
     }
 
     private ArrayList<String> toJsonList(JSONArray array){
